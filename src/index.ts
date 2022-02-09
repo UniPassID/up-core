714824f42c9a5dcb4d25728c9e490d802e77a6d0
johnz <johnz@lay2.dev>
2022-02-09 20:03:11 +0800

    add type definition

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,24 @@
  */
 import { connect, disconnect } from './connect';
 import { authorize } from './authorize';
-import config from './config';
+import config, { UPConfigOption, UP_CONFIG } from './config';
 import { initPop } from './init-pop';
+import {
+  UPAccount,
+  UPAuthMessage,
+  UPAuthResponse,
+  UPConnectOptions,
+} from './types';
 
-const up = {
+export type UPCoreType = {
+  connect: (option?: UPConnectOptions) => Promise<UPAccount>;
+  disconnect: () => void;
+  authorize: (msg: UPAuthMessage) => Promise<UPAuthResponse>;
+  config: (option: UPConfigOption) => UP_CONFIG;
+  initPop: () => void;
+};
+
+const up: UPCoreType = {
   connect,
   disconnect,
   authorize,
